a980e1fcba8aad6b9622d6b3a012159fda13b476
Manoj Upreti <manoj-u@HCL.COM>
2019-08-27 16:44:04 +0530

    updated code for ref

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*https://www.youtube.com/watch?v=tiytyGEodl0 */
+
+class App  extends Component {
+
+
+  render (){
+    this.handleSubmit = (event)=>{
+      console.log('First name is ', this.firstName.value);
+      console.log('last name is ', this.lastName.value);
+      console.log('Age is ', this.age.value);
+    }
+
+    this.inputKeyUp = (target, event)=>{
+      if(event && event.keyCode===13){
+        switch(target){
+          case 'firstName':
+              this.lastName.focus();
+            break;
+          case 'lastName':
+            this.age.focus();
+            break;
+          case 'age':
+              this.submit.focus();
+            break;
+          default:
+            break;
+        }   
+      }
+    }
+
+    return (
+      <div className="container">
+        <div><span className="form-label">First Name: </span>
+        <input type="text" placeholder="First Name"
+        ref= {(input)=>{
+          this.firstName = input;
+        }}
+        onKeyUp={this.inputKeyUp.bind(this,'firstName')}/>
+        </div>
+        <div><span className="form-label">Last Name: </span>
+        <input type="text" placeholder="Last Name"
+        ref= {(input)=>{
+          this.lastName = input;
+        }}
+        onKeyUp={this.inputKeyUp.bind(this, 'lastName')}
+        /></div>
+        <div><span className="form-label">Age: </span><input type="text" placeholder="Age"
+        ref= {(input)=>{
+          this.age = input;
+        }}
+        onKeyUp={this.inputKeyUp.bind(this, 'age')}/></div>
+        <input type="submit" value="submit" className="submit-btn"
+         onClick= {this.handleSubmit}
+         ref= {(input)=>{
+          this.submit = input;
+        }}
+        onKeyUp={this.inputKeyUp}/>        
+      </div>
+    );
+  } 
 }
 
 export default App;
